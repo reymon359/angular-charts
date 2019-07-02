857f6aa07a2c35d154e0e202526b30b9d622e7c8
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-02 12:37:31 +0200

    lie comoponent created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Charts
+import { ChartsModule } from 'ng2-charts';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LineComponent } from './components/line/line.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LineComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/line/line.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-line',
+  templateUrl: './line.component.html',
+  styles: []
+})
+export class LineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
