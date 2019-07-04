0266b616029b07759334cdc3dbc76e4009341181
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-04 07:57:58 +0200

    bar hover doughnut colors

--- src/app/components/doughnut/doughnut.component.ts
+++ src/app/components/doughnut/doughnut.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ChartType } from 'chart.js';
-import { MultiDataSet, Label } from 'ng2-charts';
+import {Color, MultiDataSet, Label } from 'ng2-charts';
 @Component({
   selector: 'app-doughnut',
   templateUrl: './doughnut.component.html',
@@ -13,6 +13,32 @@ export class DoughnutComponent {
     [350, 450, 100],
     [50, 150, 120],
     [250, 130, 70],
+  ];
+  public doughnutChartColors: Color[] = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.7)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // dark grey
+      backgroundColor: 'rgba(77,83,96,0.7)',
+      borderColor: 'rgba(77,83,96,1)',
+      pointBackgroundColor: 'rgba(77,83,96,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(77,83,96,1)'
+    },
+    { // red
+      backgroundColor: 'rgb(235, 104, 100,0.7)',
+      borderColor: '#e22620',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
   ];
   public doughnutChartType: ChartType = 'doughnut';
 
