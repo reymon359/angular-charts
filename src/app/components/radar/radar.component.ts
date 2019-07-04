e03adf619abafcc572bee02d0bb69f3255fce273
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-04 08:28:36 +0200

    Radar Component

--- src/app/components/radar/radar.component.ts
+++ src/app/components/radar/radar.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { ChartDataSets, ChartType, RadialChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 @Component({
   selector: 'app-radar',
   templateUrl: './radar.component.html',
   styles: []
 })
-export class RadarComponent implements OnInit {
+export class RadarComponent {
 
-  constructor() { }
+  public radarChartOptions: RadialChartOptions = {
+    responsive: true,
+  };
+  public radarChartLabels: Label[] = ['Eating', 'Drinking', 'Sleeping', 'Playing', 'Running', 'Jumping', 'Swimming'];
 
-  ngOnInit() {
+  public radarChartData: ChartDataSets[] = [
+    { data: [65, 59, 90, 81, 56, 55, 40], label: 'Dogs🐕' },
+    { data: [28, 48, 40, 19, 96, 27, 100], label: 'Cats🐱‍🏍' }
+  ];
+  public radarChartType: ChartType = 'radar';
+
+
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
 }
