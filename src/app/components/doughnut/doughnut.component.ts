1e5b1902595dca6202034050cfe21afd0db2100e
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-04 07:54:27 +0200

    doughnut html

--- src/app/components/doughnut/doughnut.component.ts
+++ src/app/components/doughnut/doughnut.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { ChartType } from 'chart.js';
+import { MultiDataSet, Label } from 'ng2-charts';
 @Component({
   selector: 'app-doughnut',
   templateUrl: './doughnut.component.html',
   styles: []
 })
-export class DoughnutComponent implements OnInit {
+export class DoughnutComponent {
+  // Doughnut
+  public doughnutChartLabels: Label[] = ['Dogs🐕', 'Cats🐱‍🏍', 'Tortoises🐢'];
+  public doughnutChartData: MultiDataSet = [
+    [350, 450, 100],
+    [50, 150, 120],
+    [250, 130, 70],
+  ];
+  public doughnutChartType: ChartType = 'doughnut';
 
-  constructor() { }
-
-  ngOnInit() {
+  // events
+  public chartClicked({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
   }
 
+  public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
 }
+
