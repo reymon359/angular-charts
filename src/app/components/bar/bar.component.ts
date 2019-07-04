e4e40d9290efd8525d5424a33965a1d81d072e92
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-07-04 07:41:45 +0200

    bar chart done

--- src/app/components/bar/bar.component.ts
+++ src/app/components/bar/bar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
-import { Label } from 'ng2-charts';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-bar',
@@ -22,7 +22,24 @@ export class BarComponent{
   public barChartLabels: Label[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
-
+  public barChartColors: Color[] = [
+    { // grey
+      backgroundColor: 'rgba(148,159,177,0.7)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    },
+    { // red
+      backgroundColor: 'rgb(235, 104, 100,0.7)',
+      borderColor: '#e22620',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+    }
+  ];
   public barChartData: ChartDataSets[] = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Dogs🐕' },
     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Cats🐱‍🏍' }
@@ -40,12 +57,12 @@ export class BarComponent{
     // Only Change 3 values
     const data = [
       Math.round(Math.random() * 100),
-      59,
-      80,
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
       (Math.random() * 100),
-      56,
+      Math.round(Math.random() * 100),
       (Math.random() * 100),
-      40];
+      Math.round(Math.random() * 100),];
     this.barChartData[0].data = data;
   }
 }
